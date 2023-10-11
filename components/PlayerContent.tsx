62c00226061fc094f8447ce099f64357fb6547f2
Ehong Kuo <ehongkuo@gmail.com>
2023-10-10 23:19:05 -0700

    play next song functionality

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -3,9 +3,13 @@
 import { Song } from "@/types";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import { useState } from "react";
+
+import usePlayer from "@/hooks/usePlayer";
+
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
-import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import Slider from "./Slider";
 
 interface PlayerContentProps {
@@ -16,9 +20,27 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
   song,
   songUrl
 }) => {
-  const Icon = true ? BsPauseFill : BsPlayFill;
-  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
 
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong);
+  }
   return ( 
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="
