06acd15ee333d8554650d6a912e4ab292474c16c
Ehong Kuo <ehongkuo@gmail.com>
2023-10-10 23:38:13 -0700

    song play functionality + first original feature: volume save

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -4,7 +4,8 @@ import { Song } from "@/types";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
 
 import usePlayer from "@/hooks/usePlayer";
 
@@ -22,6 +23,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 }) => {
   const player = usePlayer();
   const [volume, setVolume] = useState(1);
+  const [oldVolume, setOldVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
 
   const Icon = isPlaying ? BsPauseFill : BsPlayFill;
@@ -57,6 +59,45 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     player.setId(previousSong);
   }
 
+  const [play, { pause, sound }] = useSound(
+    songUrl,
+    {
+      volume: volume,
+      onplay: () => setIsPlaying(true),
+      onend: () => {
+        setIsPlaying(false);
+        onPlayNext();
+      },
+      onpause: () => setIsPlaying(false),
+      format: ['mp3']
+    }
+  );
+
+  useEffect(() => {
+    sound?.play();
+
+    return () => {
+      sound?.unload();
+    }
+  }, [sound]);
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
+  const toggleMute = () => {
+    if (volume == 0) {
+      setVolume(oldVolume);
+    } else {
+      setOldVolume(volume);
+      setVolume(0);
+    }
+  }
+
   return ( 
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="
@@ -81,7 +122,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         "
       >
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="
             h-10
             w-10
@@ -121,7 +162,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
           "
         />
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="
             flex
             items-center
@@ -151,11 +192,14 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       <div className="hidden md:flex w-full justify-end pr-2">
         <div className="flex items-center gap-x-2 w-[120px]">
           <VolumeIcon
-            onClick={() => {}}
+            onClick={toggleMute}
             className="cursor-pointer"
             size={34}
           />
-          <Slider />
+          <Slider 
+            value={volume}
+            onChange={(value) => setVolume(value)}
+          />
         </div>
 
       </div>
